f82c0a834e15434ec4631010c01f8e079e3c191a
Lukáš Belán <lukasbelan27@gmail.com>
2023-12-31 17:33:47 +0100

    Basic server - logger setup

---
+++ server/src/logging.c
@@ -0,0 +1,53 @@
+
+#include "logging.h"
+#include <stdlib.h>
+
+#define FORMAT_FATAL "[${DATE} ${TIME}] ${FILE}:${LINE}: *** FATAL ERROR *** ${MSG}"
+#define FORMAT_ERROR "[${DATE} ${TIME}] ${FILE}: ERROR !!! ${MSG}"
+#define FORMAT_WARNING "[${DATE} ${TIME}] ${FILE}: WARNING ! ${MSG}"
+
+#ifdef DEBUG
+#define FORMAT_TRACE "===TRACE=== [${UPTIME}] ${FILE} ${FUNCTION} ${LINE}: ${MSG}"
+#define FORMAT_DEBUG "===DEBUG=== [${UPTIME}] ${FILE}${LINE}: errno (${ERRNO}: ${ERMSG}) ${MSG}"
+#endif
+
+int init_logging()
+{
+        int rv = 1;
+
+        if_failed(cclogger_init(LOGGING_MULTIPLE_FILES, "/etc/dhcps/dhcp-log", "dhcps"), exit);
+        if_failed(cclogger_set_default_message_format("[${DATE} ${TIME}] ${FILE}:${LINE}: ${MSG}"), exit);
+        
+        cclogger_set_verbosity_level(10);
+        cclogger_reset_log_levels();
+        
+        /* Critical error */
+        if_failed(cclogger_add_log_level(true, true, CCLOG_TTY_CLR_MAG, NULL, FORMAT_FATAL, 1), exit);
+        /* Error */
+        if_failed(cclogger_add_log_level(true, true, CCLOG_TTY_CLR_RED, NULL, FORMAT_ERROR, 2), exit);
+        /* Warning */
+        if_failed(cclogger_add_log_level(true, true, CCLOG_TTY_CLR_YEL, NULL, FORMAT_WARNING, 3), exit);
+        /* Log */
+        if_failed(cclogger_add_log_level(true, false, CCLOG_TTY_CLR_DEF, NULL, NULL, 10), exit);
+        /* Info - generaly not needed, hence high verbosity level required */
+        if_failed(cclogger_add_log_level(true, false, CCLOG_TTY_CLR_DEF, NULL, NULL, 15), exit);
+        
+#ifdef DEBUG
+        /* Trace */
+        if_failed(cclogger_add_log_level(true, true, CCLOG_TTY_CLR_WHT, NULL, FORMAT_TRACE, 1), exit);
+        /* Debug */
+        if_failed(cclogger_add_log_level(true, true, CCLOG_TTY_CLR_WHT, NULL, FORMAT_DEBUG, 1), exit);
+#endif
+
+        cclog(LOG_MSG, NULL, "Logger initialised");
+
+        rv = 0;
+exit: 
+        return rv;
+}
+
+void uninit_logging()
+{
+        cclog(LOG_MSG, NULL, "Uninitialising logger");
+        cclogger_uninit();
+}
---
+++ server/src/logging.h
@@ -0,0 +1,37 @@
+#ifndef __LOGGER__H_
+#define __LOGGER__H_
+
+#include <cclog.h>
+#include <cclog_macros.h>
+
+/* default verbosity is 10*/
+enum LOG_LEVELS {
+    LOG_CRITICAL,  /* verbosity 1, critical process ending errors */
+    LOG_ERROR,     /* verbosity 2, errors that can be recovered from */
+    LOG_WARN,      /* verbosity 3, warnings */
+    LOG_MSG,       /* verbosity 10, generic log message */
+    LOG_INFO,      /* verbosity 15, information that may be needed for debugging */
+#ifdef DEBUG
+    /* These logs will only be created when DEBUG is defined. 
+     * Dont use directly with cclog macro, use macros defined below
+     */
+    LOG_TRACE,     /* verbosity 1, used to trace functions */ 
+    LOG_DEBUG,     /* verbosity 1, used to write debug messages */
+#endif
+};
+
+#ifdef DEBUG
+#define cclog_TRACE(MSG, ...) cclog(LOG_TRACE, NULL, MSG, ## __VA_ARGS__)
+#define cclog_DEBUG(MSG, ...) cclog(LOG_DEBUG, NULL, MSG, ## __VA_ARGS__)
+#else 
+#define cclog_TRACE(...)
+#define cclog_DEBUG(...)
+#endif
+
+/* Initialises logging for dhcp server */
+int init_logging();
+
+/* Deinitialises logging for dhcp server */
+void uninit_logging();
+
+#endif /* __LOGGER__H_ */
